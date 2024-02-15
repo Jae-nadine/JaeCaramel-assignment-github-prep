d48876571039ce588dfbdb2ecd3228246c659f87
Jae-Nadine <jae.nadine@icloud.com>
2024-02-14 23:35:08 -0700

    Updated Changes
    Learning to save on computer then
    commit and push on GitHub Desktop so code (what you're working on) can be in sync

--- src/com/coderscampus/GithubPrep.java
+++ src/com/coderscampus/GithubPrep.java
@@ -3,7 +3,12 @@ package com.coderscampus;
 public class GithubPrep {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// generate a random number
+		
+		// and collect user input
+		
+		// etc. lol
 
 	}
 
